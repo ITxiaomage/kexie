480fbda7d151ec3963e0c2c4ef2351ed328abe29
mgh <294913557@qq.com>
2019-10-18 10:27:32 +0800

    55

--- kexie/TuiJian/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TuijianConfig(AppConfig):
-    name = 'TuiJian'
-    verbose_name = '科协资源库'
--- kexie/TuiJian/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from .models import *
-
-class KXForm(ModelForm):
-    class Meta:
-        model=KX
-        fields = ['title','url','img','content','time','source']
